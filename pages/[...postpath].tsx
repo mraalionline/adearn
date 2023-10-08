9438e45529093ee105cbee7102a49a85aab97b7c
Mr. Aali Online Technologies <65459871+mraalionline@users.noreply.github.com>
2023-10-08 11:04:47 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -83,9 +83,7 @@ const Post: React.FC<PostProps> = (props) => {
 		<>
 			<Head>
 				<meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
@@ -99,7 +97,6 @@ const Post: React.FC<PostProps> = (props) => {
 				<title>{post.title}</title>
 			</Head>
 			<div className="post-container">
-				<script>window.location.href='https://www.highcpmrevenuegate.com/k8sr3t2bg?key=f7e078d3b1f7ca9cba6147ac455506bf';</script>
 				<h1>{post.title}</h1>
 				<img
 					src={post.featuredImage.node.sourceUrl}
